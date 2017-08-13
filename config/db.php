e51fc9f9590e8728b2f1971ad83b280125a5d812
ANDREW <gordun@smartdevelopers.eu>
2017-08-13 14:32:36 +0300

    change dbconfig

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=bintime',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=heroku_4678a6e2f034e7a',
+    'username' => 'b05326bb6b8ddb',
+    'password' => 'bb72e07c',
     'charset' => 'utf8',
 ];
