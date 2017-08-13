482de1b6631d447567aa436065727dd32abc8c4c
ANDREW <gordun@smartdevelopers.eu>
2017-08-13 20:06:57 +0300

    final database fix

--- config/db.php
+++ config/db.php
@@ -1,9 +1,10 @@
 <?php
 
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=bintime',
-    'username' => 'root',
-    'password' => 'Gordundata1996',
+    'dsn' => "mysql:host={$url["host"]};dbname=".substr($url["path"],1),
+    'username' => $url["user"],
+    'password' => 'bb72e07c',
     'charset' => 'utf8',
 ];
